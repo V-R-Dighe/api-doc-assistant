aeae2e64044c22819a53ddd35c67ae79ac285c0c
VRDighe <vaibhav@plgworks.com>
2023-09-04 18:25:11 +0530

    Added cors support

--- apiDocAssistant/settings.py
+++ apiDocAssistant/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'apiDocAssistant.urls'
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
--- apiDocAssistant/views.py
+++ apiDocAssistant/views.py
@@ -2,7 +2,9 @@ from django.http import JsonResponse, StreamingHttpResponse
 import time
 
 from .privateGPT.privateGPT import generate, generate_data
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def get_suggestions(request):
     print("request: "+ request.GET.get('query'))
     query = request.GET.get('query')
